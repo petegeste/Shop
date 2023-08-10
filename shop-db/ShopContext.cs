8716089411fa0ea4a71d7f49035d9d6221f679de
Pete Gestewitz <peter.gestewitz@turbocam.com>
2023-08-10 16:36:16 -0400

    Add shop db project

---
+++ shop-db/ShopContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shop_db
+{
+    internal class ShopContext : DbContext
+    {
+
+    }
+}
